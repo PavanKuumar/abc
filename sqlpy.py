b008f1fd21bdf8e1933028222ba5eed007b35568
PavanKuumar <79443008+PavanKuumar@users.noreply.github.com>
2021-02-22 14:17:43 +0530

    Add files via upload

---
+++ sqlpy.py
@@ -0,0 +1,18 @@
+from prettytable import PrettyTable
+myTable = PrettyTable()
+myTable.field_names =(["name","age","place"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+myTable.add_row(["pavan","45","ap"])
+
+print(myTable)
+
